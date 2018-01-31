1d562d4f5f2c6e1e4c5d785c274d9e0bbd7f2391
pandaj <mail@juliocofre.cl>
2018-01-31 15:43:08 -0300

    asdas
    asd

--- js/comunes.js
+++ js/comunes.js
@@ -1,3 +1,4 @@
+// Regiones
 var regionNames = new Array();
 regionNames["metropolitana"]= 'Región Metropolitana';
 regionNames["region1"]		= 'I Región';
@@ -16,6 +17,7 @@ regionNames["region14"]		= 'XIV Región';
 regionNames["region15"]		= 'XV Región';
 regionNames["fuera"]		= 'Fuera de Chile';
 
+// Accesos
 var accesoNames = new Array();
 accesoNames[1]	= 'Solo Ingresa Datos';
 accesoNames[3]	= 'Ingresa Datos + Ver Registros';
@@ -23,3 +25,26 @@ accesoNames[5]	= 'Ingresa Datos + Registros + Campañas';
 accesoNames[7]	= 'Control Total';
 accesoNames[9]	= 'ADMIN MASTER';
 
+// convert Hex to RGBA
+function convertHex(hex,opacity){
+	hex = hex.replace('#','');
+	var r = parseInt(hex.substring(0,2), 16);
+	var g = parseInt(hex.substring(2,4), 16);
+	var b = parseInt(hex.substring(4,6), 16);
+	var result = 'rgba('+r+','+g+','+b+','+opacity/100+')';
+	return result;
+}
+
+// Random Numbers
+function random(min,max) {
+	return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+// Fecha hoy
+function getFechaHoy() {
+	var date = new Date();
+	var mm = date.getMonth() + 1;
+  	var dd = date.getDate();
+  	var laFecha = date.getFullYear() +'-'+ (mm > 9 ? '' : '0') + mm +'-'+ (dd > 9 ? '' : '0') + dd;
+  	return laFecha;
+}
--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ function detectaConexion() {
 	states[Connection.CELL_4G]  = 'Cell 4G';
 	states[Connection.NONE]     = 'No network';
 	var conexion = states[networkState];
-	if (conexion = 'No network') {
+	if (conexion == 'No network') {
 		return false;
 	} else {
 		return true;
@@ -25,20 +25,29 @@ window.internet = true;
 
 
 // Valida Login
+window.elUsuario = false;
 function loginRedirige(pag, num) {
-	var datosUsuario = window.localStorage.getItem("usuario");
+	var datosUsuario = JSON.parse( window.localStorage.getItem("usuario") );
 	
 	// Si esta logueado y vuelve al index por error
-	if (num == 0 && datosUsuario) {
-		location.href = pag +'.html';
+	if (num == 0) {
+		if (datosUsuario) {
+			alert('Ya estás Logueado.');
+			location.href = pag +'.html';
+		}
 
 	// Si ingresa a otra pagina
 	} else {
 		if (datosUsuario) {
-			if (datosUsuario['acceso'] < num) location.href = pag +'.html';
+			if (datosUsuario.acceso < num) {
+				alert('No tienes acceso a esta página.');
+				location.href = pag +'.html';
+			}
+			window.elUsuario = datosUsuario;
 
 		// Si no hay datos del usuario
 		} else {
+			alert('No estás logueado.');
 			location.href = 'index.html';
 		}
 	}
@@ -46,35 +55,132 @@ function loginRedirige(pag, num) {
 
 
 // Accesos Menu
-function accesosMenu() {
-	var datosUsuario = window.localStorage.getItem("usuario");
-	
+function loadMenu() {
 	// Si hay datos guardados de un usuario
-	if (datosUsuario) {
+	if (window.elUsuario) {
+		$('#side-menu').html(
+			'<nav class="sidebar-nav">'+
+				'<ul class="nav">'+
+					'<li class="nav-item">'+
+						'<a id="btn-1" class="nav-link" href="home.html">'+
+							'<i class="icon-star"></i> Mis Estadisticas'+
+						'</a>'+
+					'</li>'+
+					'<li class="nav-item">'+
+						'<a id="btn-2" class="nav-link" href="registro.html">'+
+							'<i class="icon-plus"></i> Registrar Cliente'+
+						'</a>'+
+					'</li>'+
+					'<li class="nav-item acceso3">'+
+						'<a id="btn-3" class="nav-link" href="estadisticas.html">'+
+							'<i class="icon-chart"></i> Estadisticas Generales'+
+						'</a>'+
+					'</li>'+
+					'<li class="nav-title acceso3">'+
+						'Datos Detallados'+
+					'</li>'+
+					'<li class="nav-item acceso3">'+
+						'<a id="btn-4" class="nav-link" href="datos_clientes.html">'+
+							'<i class="icon-list"></i> Clientes Únicos'+
+						'</a>'+
+					'</li>'+
+					'<li class="nav-item acceso3">'+
+						'<a id="btn-5" class="nav-link" href="datos_clientes_cmp.html">'+
+							'<i class="icon-list"></i> Clientes por Campaña'+
+						'</a>'+
+					'</li>'+
+					'<li class="nav-item acceso3">'+
+						'<a id="btn-6" class="nav-link" href="datos_clientes_lgr.html">'+
+							'<i class="icon-list"></i> Clientes por Lugar'+
+						'</a>'+
+					'</li>'+
+					'<li class="nav-item acceso3">'+
+						'<a id="btn-7" class="nav-link" href="datos_clientes_usr.html">'+
+							'<i class="icon-list"></i> Clientes por Usuario'+
+						'</a>'+
+					'</li>'+
+					'<li class="nav-title acceso5">'+
+						'Administración'+
+					'</li>'+
+					'<li class="nav-item acceso7">'+
+						'<a id="btn-8" class="nav-link" href="edit_users.html">'+
+							'<i class="icon-people"></i> Admin. Usuarios'+
+						'</a>'+
+					'</li>'+
+					'<li class="nav-item acceso5">'+
+						'<a id="btn-9" class="nav-link" href="edit_campa.html">'+
+							'<i class="icon-settings"></i> Admin. Campañas'+
+						'</a>'+
+					'</li>'+
+				'</ul>'+
+			'</nav>'+
+			'<button class="sidebar-minimizer brand-minimizer" type="button"></button>'
+		);
 
-		// Borra los menus de nivel 7
-		if (datosUsuario['acceso'] < 7) {
-			$('.acceso7').each(function() {
+		// menus de nivel 7
+		if (window.elUsuario.acceso < 7) {
+			$('#side-menu .acceso7').each(function() {
 				$(this).remove();
 			});
 		}
 
-		// Borra los menus de nivel 5
-		if (datosUsuario['acceso'] < 5) {
-			$('.acceso5').each(function() {
+		// menus de nivel 5
+		if (window.elUsuario.acceso < 5) {
+			$('#side-menu .acceso5').each(function() {
 				$(this).remove();
 			});
 		}
 
-		// Borra los menus de nivel 3
-		if (datosUsuario['acceso'] < 3) {
-			$('.acceso3').each(function() {
+		// menus de nivel 3
+		if (window.elUsuario.acceso < 3) {
+			$('#side-menu .acceso3').each(function() {
 				$(this).remove();
 			});
 		}
-	
-	// Si no hay usuario
-	} else {
-		location.href = 'index.html';
+	}
+}
+
+
+// Log Out
+function logout() {
+	var confirma = confirm("¿ Realmente quieres salir ?");
+	if (confirma == true) {
+		//window.localStorage.removeItem("usuario");
+		window.localStorage.clear();
+		setTimeout(function() {
+			location.href = 'index.html';
+		}, 500);
+	}
+}
+
+
+// Carga Header
+function loadHeader() {
+	if (window.elUsuario) {
+		$('#header').html(
+			'<button class="navbar-toggler mobile-sidebar-toggler d-lg-none mr-auto" type="button">'+
+				'<span class="navbar-toggler-icon"></span>'+
+			'</button>'+
+			'<a class="navbar-brand" href="home.html">'+
+				'<img src="img/logo-sika.png" width="100%" />'+
+			'</a>'+
+			'<button class="navbar-toggler sidebar-toggler d-md-down-none" type="button">'+
+				'<span class="navbar-toggler-icon"></span>'+
+			'</button>'+
+			'<ul class="nav navbar-nav ml-auto">'+
+				'<li class="nav-item dropdown">'+
+					'<div class="nav-link dropdown-toggle nav-link">'+
+						'<span id="userdata-nombre">'+ window.elUsuario.nombre +'</span> ' +
+						'<img src="img/avatares/'+ window.elUsuario.foto +'" class="img-avatar" />'+
+					'</div>'+
+				'</li>'+
+				'<li class="nav-item d-md-down-none">'+
+					'<a class="nav-link" href="javascript: logout();">'+
+						'<i class="fa fa-lock"></i> Logout'+
+					'</a>'+
+				'</li>'+
+				'<li class="nav-item d-md-down-none"></li>'+
+			'</ul>'
+		);
 	}
 }
