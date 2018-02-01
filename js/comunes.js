e33a9de1cece38f6d4bd7ce8de91e7d728c9796c
pandaj <mail@juliocofre.cl>
2018-02-01 16:26:53 -0300

    jueves
    eso

--- js/comunes.js
+++ js/comunes.js
@@ -25,6 +25,80 @@ accesoNames[5]	= 'Ingresa Datos + Registros + Campañas';
 accesoNames[7]	= 'Control Total';
 accesoNames[9]	= 'ADMIN MASTER';
 
+
+// Carga Datos Iniciales
+var usersNames = new Array();
+var lugarNames = new Array();
+var campaNames = new Array();
+var losDatosSave = false;
+function cargaInicial(indica) {
+	if (window.elUsuario) {
+		
+		// Si hay conexion a internet
+		if (window.internet) {
+			$.ajax({
+				url	 : window.dataURL + 'datosBase.php',
+				type : 'GET',
+				data : {
+					usuario	: window.elUsuario.email,
+					password: window.elUsuario.pass,
+					tipo	: indica
+				}
+			}).done(function(txt) {
+				
+				// Si carga Datos
+				if (txt.substr(0,5) == 'Exito') {
+					var datosA = txt.split('<-xx_xx->');
+					for (var a = 1; a < datosA.length; a++) {
+						var datosB = datosA[a].split('<-x_x->');
+						var newDato = new Object();
+						for (var b = 0; b < datosB.length; b++) {
+							var datosC = datosB[b].split('<-x->');
+							newDato[ datosC[0] ] = datosC[1];
+						}
+						if (indica == 'users')	usersNames.push(newDato);
+						if (indica == 'lugar')	lugarNames.push(newDato);
+						if (indica == 'campa')	campaNames.push(newDato);
+					}
+
+					if (indica == 'campa') {
+						losDatosSave = {
+							users : usersNames,
+							lugar : lugarNames,
+							campa : campaNames
+						};
+						window.localStorage.setItem("datos_base", JSON.stringify(losDatosSave) );
+						ejecutaInicio();
+						
+					} else {
+						setTimeout(function(){
+							if (indica == 'users')		indica = 'lugar';
+							else if (indica == 'lugar')	indica = 'campa';
+							cargaInicial(indica);
+						},500);
+					}
+
+				// Error
+				} else {
+					alert(txt);
+				}
+			});
+
+		// Si no hay conexion a internet
+		} else {
+			losDatosSave = JSON.parse( window.localStorage.getItem("datos_base") );
+			if (losDatosSave) {
+				usersNames = losDatosSave.users;
+				lugarNames = losDatosSave.lugar;
+				campaNames = losDatosSave.campa;
+				ejecutaInicio();
+			} else {
+				alert('No hay datos guardados. Debes conectarte a internet.');
+			}
+		}
+	}
+}
+
 // convert Hex to RGBA
 function convertHex(hex,opacity){
 	hex = hex.replace('#','');
--- php/footer.php
+++
@@ -1,9 +0,0 @@
-	<footer class="app-footer">
-		<span>Powered by <a href="http://redon.cl" target="_blank">Redon©</a> 2018.</span>
-	</footer>
-
-	<!-- Bootstrap and necessary plugins -->
-	<script src="library/jquery/jquery-1.10.2.js"></script>
-	<script src="library/popper/popper.min.js"></script>
-	<script src="library/bootstrap/bootstrap.min.js"></script>
-	<script src="js/app.js"></script>
