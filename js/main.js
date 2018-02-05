8d8f4034e7c1397a4b47c975888d8b044faa189a
pandaj <mail@juliocofre.cl>
2018-02-05 20:00:28 -0300

    Waka

--- js/main.js
+++ js/main.js
@@ -5,17 +5,6 @@ window.dataURL = 'http://appsredon.cl/apps/sikaform/connect/';
 // Detecta si hay conexion a internet o no
 window.internet = false;
 function detectaConexion() {
-	/*
-	var networkState = navigator.network.connection.type;
-	var states = new Array();
-	states[Connection.UNKNOWN]  = 'Unknown';
-	states[Connection.ETHERNET] = 'Ethernet';
-	states[Connection.WIFI]     = 'WiFi';
-	states[Connection.CELL_2G]  = 'Cell 2G';
-	states[Connection.CELL_3G]  = 'Cell 3G';
-	states[Connection.CELL_4G]  = 'Cell 4G';
-	states[Connection.NONE]     = 'No network';
-	*/
 	var conexion = window.navigator.onLine;
 	if (conexion) {
 		window.internet = true;
